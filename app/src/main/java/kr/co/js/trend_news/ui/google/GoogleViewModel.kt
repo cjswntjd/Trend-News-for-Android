fc3c3b434a22975deaf35624c54b7c0d7b06a1d9
cheonjoosung <cjswntjd@gmail.com>
2022-10-25 22:11:10 +0900

    바텀 네비게이션 명 변경

--- app/src/main/java/kr/co/js/trend_news/ui/google/GoogleViewModel.kt
+++ app/src/main/java/kr/co/js/trend_news/ui/google/GoogleViewModel.kt
@@ -1,13 +1,13 @@
-package kr.co.js.trend_news.ui.home
+package kr.co.js.trend_news.ui.google
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class GoogleViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is google Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/kr/co/js/trend_news/ui/naver/NaverFragment.kt
+++ app/src/main/java/kr/co/js/trend_news/ui/naver/NaverFragment.kt
@@ -1,4 +1,4 @@
-package kr.co.js.trend_news.ui.notifications
+package kr.co.js.trend_news.ui.naver
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import kr.co.js.trend_news.databinding.FragmentNotificationsBinding
+import kr.co.js.trend_news.databinding.FragmentNaverBinding
 
-class NotificationsFragment : Fragment() {
+class NaverFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentNaverBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val dashboardViewModel =
+            ViewModelProvider(this).get(NaverViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentNaverBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
